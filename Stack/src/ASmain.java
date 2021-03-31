4250426482cd5a5f2894f9366f11826a02b015fd
feel2450 <74747288+feel2450@users.noreply.github.com>
2021-03-31 16:57:37 +0900

    Add files via upload

---
+++ Stack/src/ASmain.java
@@ -0,0 +1,19 @@
+
+public class ASmain {
+
+	public static void main(String[] args) {
+		ArrayStack01<String> stack = new ArrayStack01<String>();
+		
+		stack.push("apple");
+		stack.push("orange");
+		stack.push("cherry");
+		System.out.println(stack.peek());
+		stack.push("pear");
+		stack.print();
+		stack.pop();
+		System.out.println(stack.peek());
+		stack.push("grape");
+		stack.print();
+	}
+
+}
